063a78cc4c0f5dbdac55b2a6643030d2553bd20c
jemarygold <jemarygold@gmail.com>
2024-02-08 23:35:19 +0000

    Completed excercise 4 and passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...varsToRemove) {
+    const args = Array.from(varsToRemove);
+    let filteredArray = [];
 
+    for(let i = array.length - 1; i >=0; i--) {
+
+        for(let j=0; j<args.length; j++) {
+
+            if(array[i] === args[j]){
+                array.splice(i, 1, )
+            } 
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
