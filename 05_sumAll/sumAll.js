32099ce28116f3c4e6803051638b17929586a758
jemarygold <jemarygold@gmail.com>
2024-02-08 23:59:27 +0000

    Completed Exercise 5 and passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    const largestNumber = Math.max(num1, num2);
+    const smallestNumber = Math.min(num1, num2);
+    let sumAll = 0;
 
+    if(typeof(num1) != "number" || typeof(num2) != "number" || num1 <0 || num2 < 0){
+        return "ERROR";
+    } 
+
+    for(let i=smallestNumber; i<= largestNumber; i++){
+        sumAll += i;
+    }
+    return sumAll;
 };
 
 // Do not edit below this line
