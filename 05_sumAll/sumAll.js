e5d35e8c6fb65a183f791404649b4d3fc6b36026
jemarygold <jemarygold@gmail.com>
2024-02-09 00:01:59 +0000

    Reorganise variable declaration to sit below QC

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,12 @@
 const sumAll = function(num1, num2) {
-    const largestNumber = Math.max(num1, num2);
-    const smallestNumber = Math.min(num1, num2);
-    let sumAll = 0;
-
     if(typeof(num1) != "number" || typeof(num2) != "number" || num1 <0 || num2 < 0){
         return "ERROR";
     } 
 
+    const largestNumber = Math.max(num1, num2);
+    const smallestNumber = Math.min(num1, num2);
+    let sumAll = 0;
+    
     for(let i=smallestNumber; i<= largestNumber; i++){
         sumAll += i;
     }
