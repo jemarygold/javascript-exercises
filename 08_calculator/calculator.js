c09033435018750da856eedbb6a44041d85a736b
jemarygold <jemarygold@gmail.com>
2024-02-10 01:14:06 +0000

    Completed exercise 8 and passed all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sumOfArray = 0;
+	array.forEach((element) => {sumOfArray += element})
+  return sumOfArray;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multipleOfArray = 1;
+	array.forEach((element) => {multipleOfArray *= element})
+  return multipleOfArray;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+	for (var i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+
+  return factorial;
+
 };
 
 // Do not edit below this line
