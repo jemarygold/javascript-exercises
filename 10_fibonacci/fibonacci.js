2ff1597f4c330f5c62e2667c72eb3e34e2989d96
jemarygold <jemarygold@gmail.com>
2024-02-10 09:22:59 +0000

    Completed exercise 10 and passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let integer = number * 1;
 
+    if (integer < 0) return "OOPS"
+
+    const recursiveFibonacci = function (n) {
+        if (n == 0) {
+          return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+        }
+      };
+
+    return recursiveFibonacci(integer);
 };
 
 // Do not edit below this line
