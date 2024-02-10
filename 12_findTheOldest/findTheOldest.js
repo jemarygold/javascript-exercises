9367939ecd17125c4aaa362c2a49e844876464b1
jemarygold <jemarygold@gmail.com>
2024-02-10 11:24:34 +0000

    Completed exercise 12 and passed all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    const age = function getAge(birthYear, deathYear) {
+        if (deathYear == null) {
+            deathYear = new Date().getFullYear();
+        } 
+        return deathYear - birthYear;
+    }
+
+    const person = people.reduce((prev, current) => ((age(prev.yearOfBirth, prev.yearOfDeath) > age(current.yearOfBirth, current.yearOfDeath)) ? prev : current))
+    return person;
 };
 
 // Do not edit below this line
