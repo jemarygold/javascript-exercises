06add3521cc15f50ac8a0627d4048d1f09158e16
jemarygold <jemarygold@gmail.com>
2024-02-10 01:27:45 +0000

    Completed exercise 9 and passed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,12 @@
-const palindromes = function () {
+const palindromes = function (potentialPalindrome) {
+    let palindrome = potentialPalindrome.replace(/[^\w]/g,"").toLowerCase();
+    let reversePalindrome = palindrome.split("").reverse().join("");
+
+    if (palindrome === reversePalindrome) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
